e4649caec6236ed244cb4a68858ccd0ef70ff042
Hannah Park <ha3158987@gmail.com>
2021-08-03 12:10:43 +0900

    add: 대문자로 통일 (21.08.03)

---
+++ 인프런JS알고리즘/대문자로통일.js
@@ -0,0 +1,42 @@
+/*
+입력되는 모든 글자를 대문자로 바꾸어 출력한다.
+
+기억하기!!!
+아스키코드로 변환해서 97 ~ 122 사이라면 소문자!!
+대문자는 아스키코드로 65 ~ 90 까지
+각 대문자와 소문자는 "32"만큼 차이가 남.
+*/
+
+//방법1.
+function solution (str) {
+  let answer = '';
+  str.split('').forEach(letter => {
+    answer += letter.toUpperCase()
+  })
+  return answer;
+}
+
+//방법2.
+function solution (str) {
+  let answer = '';
+  str.split('').forEach(letter => {
+    let aski = letter.charCodeAt();
+    if (aski >= 97 && aski <=122) answer += String.fromCharCode(aski - 32);
+    else answer += letter;
+  })
+  return answer;
+}
+
+
+//방법3.
+function solution (str) {
+  let answer = '';
+  str.split('').forEach(num => {
+    if (num === num.toLowerCase()) answer += num.toUpperCase();
+    else answer += num;
+  })
+  return answer;
+}
+
+
+console.log(solution('ItisTimeToStudy'));
